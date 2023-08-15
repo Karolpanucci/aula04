32819fcc7598b51c16fc30a61cbc82a4adc5da64
Karolpanucci <karolainny.panucci@estudante.ifms.edu.br>
2023-08-15 19:47:20 -0400

    atualizando o js

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,39 @@
 'use server'
+let lista = [
+    {
+        nome: "karol",
+        password:"1212",
+        email:"karol@gmail.com",
+        token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        nome: "karol",
+        password:"1212",
+        email:"marcelino@gmail.com",
+        token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        nome: "karol",
+        password:"1212",
+        email:"isa@gmail.com",
+        token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    }
+]
 
-const getUserAuthenticated = (user) => {
+const getUserAuthenticated = (userlogin) => {
+    let userAuth= {};
+    lista.map((user)=>{
+        if(user.email === userlogin.email && user.password === userlogin.password){
+            userAuth= user
+        }
+    })
+     return userAuth;
+
 
 }
 
 const getUsers = () =>{
+    return lista;
         
 }
 export { getUsers, getUserAuthenticated };
