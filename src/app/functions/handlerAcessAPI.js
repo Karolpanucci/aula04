6654cf17c3caa65312b06b557ceec4d78b24f05c
isapires <piresisabela211@gmail.com>
2023-09-05 16:58:13 -0400

    dia 05 de setembro

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -7,21 +7,21 @@ let lista = [
         token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     },
     {
-        nome: "karol",
-        password:"1212",
+        nome: "natasha",
+        password:"1313",
         email:"marcelino@gmail.com",
         token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     },
     {
-        nome: "karol",
-        password:"1212",
+        nome: "isabela",
+        password:"1414",
         email:"isa@gmail.com",
         token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
     }
 ]
 
 const getUserAuthenticated = (userlogin) => {
-    let userAuth= {};
+    let userAuth= {}; 
     lista.map((user)=>{
         if(user.email === userlogin.email && user.password === userlogin.password){
             userAuth= user
