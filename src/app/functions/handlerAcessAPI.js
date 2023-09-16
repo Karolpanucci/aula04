9afa60f08895698c861da1b3a5ac34fd77181f94
karolpanucci <karolainny.panucci@estudante.ifms.edu.br>
2023-09-15 21:51:49 -0400

    ta indo

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -20,6 +20,34 @@ let lista = [
     }
 ]
 
+let filmes = [
+    
+    {
+        nome: "de volta pro futuro",
+        estilo:"1212",
+        resumo:"karol@gmail.com",
+       image:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        nome: "alem da janela",
+        estilo:"1212",
+        resumo:"karol@gmail.com",
+       image:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        nome: "karol",
+        estilo:"1212",
+        resumo:"karol@gmail.com",
+       image:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        nome: "karol",
+        estilo:"1212",
+        resumo:"karol@gmail.com",
+       image:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+]
+
 const getUserAuthenticated = (userlogin) => {
     let userAuth= {}; 
     lista.map((user)=>{
@@ -36,4 +64,8 @@ const getUsers = () =>{
     return lista;
         
 }
-export { getUsers, getUserAuthenticated };
+const getFilme = () =>{
+    return filmes;
+        
+}
+export { getUsers, getUserAuthenticated, getFilme };
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,13 +1,13 @@
-import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { getFilme } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
 import ListUsers from "@/app/componente/ListUsers";
 
 export default async function Dashboard() {
-    const lista = getUsers()
+    const filmes = getFilme()
     return (
         <div>
             <Suspense fallback={<p>carregando...</p>}>
-                <ListUsers users={lista} />
+                <ListUsers users={filmes} />
                 <a href="/pages/alterar">Altera ai pow</a>
                 <a href="/pages/registrar">Registra ai mano</a>
             </Suspense>
