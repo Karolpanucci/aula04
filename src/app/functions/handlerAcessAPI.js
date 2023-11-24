43b44bbf5a5c31897d3dc9bc686f975d2fa58231
Karolpanucci <karolainny.panucci@estudante.ifms.edu.br>
2023-11-24 16:38:56 -0400

    commit

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,7 +1,7 @@
 'use server'
 
 
-const url = "https://service-work-rdsvz1sd0-karolpanucci.vercel.app/"
+const url = "https://service-work-p-tj87.vercel.app"
 
 
 const getUserAuthenticated = async (user) => {
--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,10 +1,11 @@
-import { getUsers } from "@/app/functions/handlerAcessAPI";
+import {getUsers} from "@\app\functions\handlerAcessAPI"
 import { Suspense } from "react";
 import ListUsers from "@/app/componente/ListUsers";
 import './estilo.css';
 
 export default async function Dashboard() {
-    const users = await getUsers();
+    //const users = await getUsers();
+    const usuarios= await getUsers 
   return(
     <div>
         <Suspense fallback={<p>carregando...</p>}>
